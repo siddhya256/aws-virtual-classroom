b49e174096151174b37325bb9bc6204875d82cc4
siddhya256 <siddheshchougule256@gmail.com>
2026-06-01 22:43:52 +0530

    Updated home page and UI

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "AWS Virtual Classroom Running"
+    return render_template('home.html')
 
 def get_db_connection():
     conn = mysql.connector.connect(
